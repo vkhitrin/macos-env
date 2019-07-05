f9d883bd907a2ef614a083df59d3094526252a79
Vadim Khitrin <me@vkhitrin.com>
2019-07-05 20:56:44 +0300

    Installation: Change installation method
    Use brew bundle with Brewfile to install brew components

--- Setup/install.sh
+++ Setup/install.sh
@@ -6,20 +6,16 @@ function error_exit {
 
 # Make sure script isn't executed on non macOS systems
 if [[ $(uname) != "Darwin" ]];then
-    echo "Please make sure you're running on macOS"
-    exit 1
+    error_exit "Please make sure you're running on macOS"
 fi
 
 # Verify brew is installed
 which brew > /dev/null 2>/dev/null || error_exit "Brew is not installed, please download from https://brew.sh/"
 
-# Verify existence of recipe file
-[ -f ./Setup/install_recipe.txt ] || error_exit "No recipe file is found"
-
-# Itterate over recipe file
-while read command; do
-    echo "Executing '$command'"; $command
-done <./Setup/install_recipe.txt
+# Verify existence of Brewfile
+[ -f ./Setup/Brewfile ] || error_exit "No Brewfile is found"
+echo "Installing brew taps/casks:"
+brew bundle --file=./Setup/Brewfile
 
 # Download Vim Plug
 [ -d ~/.local/share/nvim ] || curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
