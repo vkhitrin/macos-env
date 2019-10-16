94fe193b2fed3b4bc68a2f9af0446e0fe26f19b2
Vadim Khitrin <me@vkhitrin.com>
2019-10-16 23:35:09 +0300

    mackup: Sync itsycal
    Remove itsycal configuration from defualts due to an issue with the app
    being unusable after setting defaults and restarting.
    
    Configure to backup itsycal using mackup.

--- Setup/macos_defaults.sh
+++ Setup/macos_defaults.sh
@@ -9,28 +9,12 @@ if [[ $ACTION == 'install' ]];then
     defaults write com.apple.finder AppleShowAllFiles TRUE
     echo "Restarting Finder"
     killall Finder
-    echo "Writing itsycal defaults"
-    defaults write com.mowglii.ItsycalApp ClockFormat 'EEEE, d MMMM, HH:mm:ss'
-    defaults write com.mowglii.ItsycalApp HideIcon 1
-    defaults write com.mowglii.ItsycalApp HighlightedDOWs 96
-    defaults write com.mowglii.ItsycalApp ShowEventDays 7
-    defaults write com.mowglii.ItsycalApp ShowLocation 1
-    defaults write com.mowglii.ItsycalApp ShowWeeks 1
-    defaults write com.mowglii.ItsycalApp SizePreference 1
 elif [[ $ACTION == 'uninstall' ]]; then
     echo "Deleting macOS defaults"
     defaults delete com.apple.screencapture disable-shadow
     defaults delete com.apple.finder AppleShowAllFiles
     echo "Restarting Finder"
     killall Finder
-    echo "Deleting itsycal defaults"
-    defaults delete com.mowglii.ItsycalApp ClockFormat
-    defaults delete com.mowglii.ItsycalApp HideIcon
-    defaults delete com.mowglii.ItsycalApp HighlightedDOWs
-    defaults delete com.mowglii.ItsycalApp ShowEventDays
-    defaults delete com.mowglii.ItsycalApp ShowLocation
-    defaults delete com.mowglii.ItsycalApp ShowWeeks
-    defaults delete com.mowglii.ItsycalApp SizePreference
 else
     echo "Accepted arguments: 'install', 'uninstall'"
 fi
