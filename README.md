2dcca90414547f40f1392c663c86629a904260ec
Vadim Khitrin <me@vkhitrin.com>
2024-05-31 15:31:02 +0300

    feature(install): Update Brew + Scripts

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # macos-env
 
-Personal dotfiles and tools.
+> [!NOTE]
+> The files in this repository are relevant to my personal workflow!
 
-**Note:** The files in this repository are relevant to my personal workflow!
+Personal configuration and tooling.
+
+Dotfiles are stored externally in a separate repository [vkhitrin/dotfiles](https://github.com/vkhitrin/dotfiles).
 
 ## Installation
 
 Use `make` command to install relevant components.
-
-Dotfiles are stored externally in a separate repository [vkhitrin/dotfiles](https://github.com/vkhitrin/dotfiles).
--- scripts/install-extra.sh
+++ scripts/install-extra.sh
@@ -30,7 +30,7 @@ $HOMEBREW_PATH_PREFIX/pip3 install --break-system-packages --upgrade passhole ha
 # Kubernetes krew plugins
 print_padded_title "krew - Install Kubernetes CLI Extensions Using krew"
 if [ -f /opt/homebrew/bin/kubectl-krew ]; then
-    kubectl krew install browse-pvc cert-manager ctr deprecations dup get-all graph history images node-admin node-logs node-restart node-shell nodepools resource-capacity restart sick-pods unlimited view-secret viewnode
+    kubectl krew install browse-pvc cert-manager ctr deprecations dup get-all graph history images node-admin node-logs node-restart node-shell nodepools resource-capacity restart sick-pods unlimited view-secret viewnode tree
 fi
 
 # helm plugins
