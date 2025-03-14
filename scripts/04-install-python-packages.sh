4c1ecb8d535382f4fa35511993f6bfdf831141fb
Vadim Khitrin <me@vkhitrin.com>
2025-03-14 20:33:57 +0200

    chore: Update mackup And Scripts
    Updating mackup path.
    
    Replacing `pipx` with `uv` for installing python scripts.

--- scripts/04-install-python-packages.sh
+++ scripts/04-install-python-packages.sh
@@ -3,17 +3,12 @@ set -eo pipefail
 
 source ./scripts/common.sh
 
-print_padded_title "Brew pipx - Install Additional Software"
-declare -a PIPX_PACKAGES
-declare -a PIPX_HARLEQUIN_PACKAGES
-# NOTE: Install harlequin from source while support for Python 3.13 is not shipped.
-#PIPX_PACKAGES=("passhole" "harlequin" "scylla-cqlsh" "rexi")
-PIPX_PACKAGES=("passhole" "git+https://github.com/tconbeer/harlequin" "scylla-cqlsh" "rexi")
-# NOTE: harlequin-cassandra does not work on Python 3.13 due to datastax driver
-# PIPX_HARLEQUIN_PACKAGES=("catppuccin[pygments]" "boto3" "harlequin-postgres" "harlequin-mysql" "harlequin-odbc" "harlequin-cassandra" "git+https://github.com/ThomAub/harlequin-clickhouse")
-PIPX_HARLEQUIN_PACKAGES=("boto3" "harlequin-postgres" "harlequin-mysql" "harlequin-odbc")
-${HOMEBREW_BIN_PATH_PREFIX}/pipx install "${PIPX_PACKAGES[@]}"
-for HARLEQUIN_PACKAGE in "${PIPX_HARLEQUIN_PACKAGES[@]}"; do
-   ${HOMEBREW_BIN_PATH_PREFIX}/pipx inject harlequin "${HARLEQUIN_PACKAGE}"
+print_padded_title "Brew uv - Install Additional Software"
+declare -a UV_PACKAGES
+declare -a UV_HARLEQUIN_EXTRA_ARGS
+UV_PACKAGES=("passhole")
+UV_HARLEQUIN_EXTRA_ARGS=("--with boto3" "--with harlequin-postgres" "--with harlequin-mysql" "--with harlequin-odbc")
+for UV_PACKAGE in "${UV_PACKAGES[@]}"; do
+    ${HOMEBREW_BIN_PATH_PREFIX}/uv tool install --refresh "${UV_PACKAGE}"
 done
-${HOMEBREW_BIN_PATH_PREFIX}/pipx upgrade-all --include-injected
+${HOMEBREW_BIN_PATH_PREFIX}/uv tool install --refresh harlequin ${UV_HARLEQUIN_EXTRA_ARGS[@]}
